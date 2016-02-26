61debdf54aee085052416766ceea1c8af9e42513
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-02-26 16:34:00 -0700

    fix(PipelineBrowser): Add ColorBy section

--- src/panels/ControlPanel/Content/PipelineBrowser/index.js
+++ src/panels/ControlPanel/Content/PipelineBrowser/index.js
@@ -1,6 +1,7 @@
 import React                from 'react';
-import ProxyEditorWidget    from 'paraviewweb/src/React/Widgets/ProxyEditorWidget';
+import ColorByWidget        from 'paraviewweb/src/React/Widgets/ColorByWidget';
 import PipelineWidget       from 'paraviewweb/src/React/Widgets/GitTreeWidget';
+import ProxyEditorWidget    from 'paraviewweb/src/React/Widgets/ProxyEditorWidget';
 import style                from 'VisualizerStyle/PipelineBrowser.mcss';
 
 function getIndexedProxyMap(proxyList) {
@@ -9,6 +10,10 @@ function getIndexedProxyMap(proxyList) {
         idToProxyMap[proxy.id] = proxy;
     });
     return idToProxyMap;
+}
+
+function eventNotHandled(e) {
+    console.log('Event not handled', e);
 }
 
 export default React.createClass({
@@ -33,6 +38,8 @@ export default React.createClass({
             sourceProxy: null,
             representationProxy: null,
             viewProxy: null,
+            min: 0,
+            max: 1,
         };
     },
 
@@ -44,6 +51,66 @@ export default React.createClass({
             this.updateActiveProxy(id, 'Source', 'sourceProxy');
         });
         this.updateProxyList();
+
+        // Do not freeze important requests
+        setTimeout( () => {
+            this.props.proxyManager.listColorMapImages()
+                .then(presets => {
+                    this.setState({presets});
+                });
+        }, 500);
+
+        this.colorHandler = {
+            propertyChange: ({changeSet}) => {
+                this.props.proxyManager.updateProperties(changeSet)
+                    .then(
+                        ok => {
+                            this.updateActiveProxy(this.state.sourceProxy.id, 'Source', 'sourceProxy');
+                        });
+            },
+            colorBy: ({representation, arrayLocation, arrayName, colorMode, vectorMode, vectorComponent, rescale}) => {
+                this.props.proxyManager.colorBy(representation, colorMode, arrayLocation, arrayName, vectorComponent, vectorMode, rescale)
+                    .then(
+                        ok => {
+                            console.log('Update colorBy', ok);
+                        },
+                        err => {
+                            console.log('Error update color', err);
+                        });
+            },
+            scalarBar: ({source, visible}) => {
+                this.props.proxyManager.showScalarBar(source, visible)
+                    .then(
+                        ok => {
+                            // console.log('update bar visibility', ok);
+                        },
+                        err => {
+                            console.log('Error update bar visibility', err);
+                        }
+                    );
+            },
+            updatePreset: ({representation, preset}) => {
+                this.props.proxyManager.updateLookupTablePreset(representation, preset)
+                    .then(
+                        ok => {
+                            this.updateRepresentationProxy(representation);
+                        },
+                        err => {
+                            console.log('Error updating preset', err);
+                        });
+            },
+            updateScalarRange: ({options}) => {
+                this.props.proxyManager.rescaleTransferFunction(options)
+                    .then(
+                        ok => {
+                            const {min,max} = ok.range;
+                            this.setState({min,max});
+                        },
+                        err => {
+                            console.log('Error updateScalarRange', err);
+                        });
+            },
+        };
     },
 
     componentWillUnmount() {
@@ -75,18 +142,42 @@ export default React.createClass({
     },
 
     updateActiveProxy(id, name, key) {
-        const collapsed = false;
+        const collapsed = (key === 'viewProxy');
         this.props.proxyManager.getProxy(id, true)
             .then(
                 proxy => {
                     this.setState({ [key]: Object.assign({name, collapsed}, proxy) });
                     if(key === 'sourceProxy' && this.state.proxyMap[proxy.id]) {
-                        this.updateActiveProxy(this.state.proxyMap[proxy.id].rep, 'Representation', 'representationProxy');
+                        this.props.proxyManager.getLookupTableScalarRange(proxy.id)
+                            .then(
+                                ok => {
+                                    const { min, max } = ok;
+                                    this.setState({min, max});
+                                },
+                                err => {
+                                    console.log('Error fetching LUT scalar range', err);
+                                });
+
+                        this.updateRepresentationProxy(this.state.proxyMap[proxy.id].rep);
                     }
                 },
                 ko => {
                     console.log('error getProxy', name, id, ko);
                 });
+    },
+
+    updateRepresentationProxy(id) {
+        this.props.proxyManager.getLookupTableImage(id)
+            .then(
+                ok => {
+                    const scalarBar = ok.image;
+                    this.setState({scalarBar});
+                },
+                () => {
+                    // console.log('No lut for', this.state.proxyMap[proxy.id].rep);
+                });
+
+        this.updateActiveProxy(id, 'Representation', 'representationProxy');
     },
 
     applyChanges(changeSet) {
@@ -157,21 +248,40 @@ export default React.createClass({
         }
     },
 
+    updateColorBy(event) {
+        const fn = this.colorHandler[event.type] || eventNotHandled;
+        fn(event);
+    },
+
     render() {
         if(!this.props.visible) {
             return null;
         }
         const sections = [this.state.sourceProxy, this.state.representationProxy, this.state.viewProxy].filter(i => !!i);
 
-        // name, id, parent, visible
-        return <div className={ style.container }>
+        return (
+            <div className={ style.container }>
+                <div className={ style.pipelineContainer }>
                 <PipelineWidget
                     nodes={ this.state.proxyList.sources }
                     actives={ [ this.props.proxyManager.getActiveProxyId() ] }
                     onChange={ this.handleChange }
                     enableDelete
-                    width='300' />
-                <ProxyEditorWidget sections={sections} onApply={ this.applyChanges }/>
-                </div>;
+                    width='295' />
+                </div>
+                <div className={ style.proxyEditorContainer }>
+                    <ProxyEditorWidget sections={sections} onApply={ this.applyChanges }>
+                        <ColorByWidget
+                            className={ style.colorBy }
+                            source={this.state.sourceProxy}
+                            representation={this.state.representationProxy}
+                            scalarBar={ this.state.scalarBar }
+                            presets= { this.state.presets }
+                            min={ this.state.min }
+                            max={ this.state.max }
+                            onChange={ this.updateColorBy }/>
+                    </ProxyEditorWidget>
+                </div>
+            </div>);
     },
 });
